df19fa848632d846f0ae1a707c246fe19fc145bc
Akash <aakashagur57@gmail.com>
2023-03-24 08:02:36 +0530

    k8s yaml file

---
+++ kubectl.run.sh
@@ -0,0 +1,4 @@
+for i in  aboutus.yaml contact.yaml course.yaml exams.yaml home.yaml login.yaml mongo.yaml nodeapp.yaml aboutus.svc.yaml contact.svc.yaml courses.svc.yaml exams.svc.yaml home.svc.yaml login.svc.yaml mongo.svc.yaml nodeapp.svc.yaml ingress.yaml
+do 
+  kubectl apply -f $i
+done
