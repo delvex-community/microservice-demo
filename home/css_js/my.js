99efd9aacbe1dd64df2f99a042a0f5d89ed4d417
Harsh Vardhan <hvkataria12@gmail.com>
2023-04-09 23:32:24 +0530

    HalfWay Through

--- home/css_js/my.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const instanceIP = window.location.hostname;
-  const dynamicLinks = document.querySelectorAll('.dynamic-href');
-  dynamicLinks.forEach(link => {
-    link.href = `http://${instanceIP}${link.getAttribute('data-href')}`;
-  });
-});
-
